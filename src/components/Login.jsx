e1f023d7ce3f96d8d17425d0dca1ace7a3ae3c29
hayyantahirr <hayyantahir7@gmail.com>
2025-08-13 00:37:42 +0500

    Login and Register Modals Added

---
+++ src/components/Login.jsx
@@ -0,0 +1,259 @@
+import { signInWithEmailAndPassword } from "firebase/auth";
+import React, { useRef, useState } from "react";
+import { useNavigate } from "react-router";
+import { auth } from "../config/Firebase/firebase";
+
+const Login = ({ setModalType }) => {
+  const email = useRef();
+  const password = useRef();
+  const navigate = useNavigate();
+  const [showPassword, setShowPassword] = useState(false);
+  const signInUser = (e) => {
+    e.preventDefault();
+    console.log(email.current.value, password.current.value);
+    // Sign in existing User Through Firebase
+
+    signInWithEmailAndPassword(
+      auth,
+      email.current.value,
+      password.current.value
+    )
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        console.log(user);
+        console.log("user Signed In Successfully");
+        navigate("/");
+      })
+      .catch((error) => {
+        const errorMessage = error.message;
+        console.log(errorMessage);
+      });
+    // closing modale
+    setModalType(null);
+  };
+
+  //   hide password functionality
+
+  return (
+    <>
+      <form
+        className="relative py-3 sm:max-w-xl sm:mx-auto "
+        onSubmit={signInUser}
+      >
+        <div className="relative px-4 py-10 bg-white mx-8 md:mx-0 shadow rounded-3xl sm:p-10 border">
+          <div className="max-w-md mx-auto">
+            <div className="flex items-center space-x-5 justify-center">
+              <h1 className="text-2xl">Sign In</h1>
+            </div>
+            <div className="mt-5">
+              <label className="font-semibold text-sm text-gray-600 pb-1 block">
+                E-mail
+              </label>
+              <input
+                className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
+                type="text"
+                ref={email}
+              />
+              <label className="font-semibold text-sm text-gray-600 pb-1 block">
+                Password
+              </label>
+              {/* password show hide work  */}
+              <div className="relative w-full mt-1 mb-5">
+                <input
+                  type={showPassword ? "text" : "password"}
+                  className="border rounded-lg px-3 py-2 pr-10 w-full text-sm"
+                  ref={password}
+                />
+                <button
+                  type="button"
+                  onClick={() => setShowPassword(!showPassword)}
+                  className="absolute inset-y-0 right-2 flex items-center text-gray-600 hover:text-gray-900"
+                >
+                  {showPassword ? (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-5 w-5"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                      />
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+                      />
+                    </svg>
+                  ) : (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-5 w-5"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M13.875 18.825A10.05 10.05 0 0112 19c-4.477 0-8.268-2.943-9.542-7a9.969 9.969 0 012.502-3.469M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                      />
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M3 3l18 18"
+                      />
+                    </svg>
+                  )}
+                </button>
+              </div>
+            </div>
+            <div className="text-right mb-4">
+              <a
+                className="text-xs font-display font-semibold text-gray-500 hover:text-gray-600 cursor-pointer"
+                href="#"
+              >
+                Forgot Password?
+              </a>
+            </div>
+            <div className="flex justify-center w-full items-center">
+              <div>
+                <button className="flex items-center justify-center py-2 px-20 bg-white hover:bg-gray-200 focus:ring-blue-500 focus:ring-offset-blue-200 text-gray-700 w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg">
+                  <svg
+                    viewBox="0 0 24 24"
+                    height="25"
+                    width="25"
+                    y="0px"
+                    x="0px"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M12,5c1.6167603,0,3.1012573,0.5535278,4.2863159,1.4740601l3.637146-3.4699707 C17.8087769,1.1399536,15.0406494,0,12,0C7.392395,0,3.3966675,2.5999146,1.3858032,6.4098511l4.0444336,3.1929321 C6.4099731,6.9193726,8.977478,5,12,5z"
+                      fill="#F44336"
+                    ></path>
+                    <path
+                      d="M23.8960571,13.5018311C23.9585571,13.0101929,24,12.508667,24,12 c0-0.8578491-0.093689-1.6931763-0.2647705-2.5H12v5h6.4862061c-0.5247192,1.3637695-1.4589844,2.5177612-2.6481934,3.319458 l4.0594482,3.204834C22.0493774,19.135437,23.5219727,16.4903564,23.8960571,13.5018311z"
+                      fill="#2196F3"
+                    ></path>
+                    <path
+                      d="M5,12c0-0.8434448,0.1568604-1.6483765,0.4302368-2.3972168L1.3858032,6.4098511 C0.5043335,8.0800171,0,9.9801636,0,12c0,1.9972534,0.4950562,3.8763428,1.3582153,5.532959l4.0495605-3.1970215 C5.1484375,13.6044312,5,12.8204346,5,12z"
+                      fill="#FFC107"
+                    ></path>
+                    <path
+                      d="M12,19c-3.0455322,0-5.6295776-1.9484863-6.5922241-4.6640625L1.3582153,17.532959 C3.3592529,21.3734741,7.369812,24,12,24c3.027771,0,5.7887573-1.1248169,7.8974609-2.975708l-4.0594482-3.204834 C14.7412109,18.5588989,13.4284058,19,12,19z"
+                      fill="#00B060"
+                    ></path>
+                    <path
+                      opacity=".1"
+                      d="M12,23.75c-3.5316772,0-6.7072754-1.4571533-8.9524536-3.7786865C5.2453613,22.4378052,8.4364624,24,12,24 c3.5305786,0,6.6952515-1.5313721,8.8881226-3.9592285C18.6495972,22.324646,15.4981079,23.75,12,23.75z"
+                    ></path>
+                    <polygon
+                      opacity=".1"
+                      points="12,14.25 12,14.5 18.4862061,14.5 18.587492,14.25"
+                    ></polygon>
+                    <path
+                      d="M23.9944458,12.1470337C23.9952393,12.0977783,24,12.0493774,24,12 c0-0.0139771-0.0021973-0.0274658-0.0022583-0.0414429C23.9970703,12.0215454,23.9938965,12.0838013,23.9944458,12.1470337z"
+                      fill="#E6E6E6"
+                    ></path>
+                    <path
+                      opacity=".2"
+                      d="M12,9.5v0.25h11.7855721c-0.0157471-0.0825195-0.0329475-0.1680908-0.0503426-0.25H12z"
+                      fill="#FFF"
+                    ></path>
+                    <linearGradient
+                      gradientUnits="userSpaceOnUse"
+                      y2="12"
+                      y1="12"
+                      x2="24"
+                      x1="0"
+                      id="LxT-gk5MfRc1Gl_4XsNKba_xoyhGXWmHnqX_gr1"
+                    >
+                      <stop stopOpacity=".2" stopColor="#fff" offset="0"></stop>
+                      <stop stopOpacity="0" stopColor="#fff" offset="1"></stop>
+                    </linearGradient>
+                    <path
+                      d="M23.7352295,9.5H12v5h6.4862061C17.4775391,17.121582,14.9771729,19,12,19 c-3.8659668,0-7-3.1340332-7-7c0-3.8660278,3.1340332-7,7-7c1.4018555,0,2.6939087,0.4306641,3.7885132,1.140686 c0.1675415,0.1088867,0.3403931,0.2111206,0.4978027,0.333374l3.637146-3.4699707L19.8414307,2.940979 C17.7369385,1.1170654,15.00354,0,12,0C5.3725586,0,0,5.3725586,0,12c0,6.6273804,5.3725586,12,12,12 c6.1176758,0,11.1554565-4.5812378,11.8960571-10.4981689C23.9585571,13.0101929,24,12.508667,24,12 C24,11.1421509,23.906311,10.3068237,23.7352295,9.5z"
+                      fill="url(#LxT-gk5MfRc1Gl_4XsNKba_xoyhGXWmHnqX_gr1)"
+                    ></path>
+                    <path
+                      opacity=".1"
+                      d="M15.7885132,5.890686C14.6939087,5.1806641,13.4018555,4.75,12,4.75c-3.8659668,0-7,3.1339722-7,7 c0,0.0421753,0.0005674,0.0751343,0.0012999,0.1171875C5.0687437,8.0595093,8.1762085,5,12,5 c1.4018555,0,2.6939087,0.4306641,3.7885132,1.140686c0.1675415,0.1088867,0.3403931,0.2111206,0.4978027,0.333374 l3.637146-3.4699707l-3.637146,3.2199707C16.1289062,6.1018066,15.9560547,5.9995728,15.7885132,5.890686z"
+                    ></path>
+                    <path
+                      opacity=".2"
+                      d="M12,0.25c2.9750366,0,5.6829224,1.0983887,7.7792969,2.8916016l0.144165-0.1375122 l-0.110014-0.0958166C17.7089558,1.0843592,15.00354,0,12,0C5.3725586,0,0,5.3725586,0,12 c0,0.0421753,0.0058594,0.0828857,0.0062866,0.125C0.0740356,5.5558472,5.4147339,0.25,12,0.25z"
+                      fill="#FFF"
+                    ></path>
+                  </svg>
+                  <span className="ml-2">Sign in with Google</span>
+                </button>
+                <button className="flex items-center justify-center py-2 px-20 bg-white hover:bg-gray-200 focus:ring-blue-500 focus:ring-offset-blue-200 text-gray-700 w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg mt-4">
+                  <svg
+                    xmlnsXlink="http://www.w3.org/1999/xlink32"
+                    xmlns="http://www.w3.org/2000/svg"
+                    version="1.1"
+                    viewBox="0 0 64 64"
+                    height="32px"
+                    width="24px"
+                  >
+                    <g
+                      fillRule="evenodd"
+                      fill="none"
+                      strokeWidth="1"
+                      stroke="none"
+                    >
+                      <g
+                        fillRule="nonzero"
+                        transform="translate(3.000000, 3.000000)"
+                      >
+                        <circle
+                          r="29.4882047"
+                          cy="29.4927506"
+                          cx="29.5091719"
+                          fill="#3C5A9A"
+                        ></circle>
+                        <path
+                          fill="#FFFFFF"
+                          d="M39.0974944,9.05587273 L32.5651312,9.05587273 C28.6886088,9.05587273 24.3768224,10.6862851 24.3768224,16.3054653 C24.395747,18.2634019 24.3768224,20.1385313 24.3768224,22.2488655 L19.8922122,22.2488655 L19.8922122,29.3852113 L24.5156022,29.3852113 L24.5156022,49.9295284 L33.0113092,49.9295284 L33.0113092,29.2496356 L38.6187742,29.2496356 L39.1261316,22.2288395 L32.8649196,22.2288395 C32.8649196,22.2288395 32.8789377,19.1056932 32.8649196,18.1987181 C32.8649196,15.9781412 35.1755132,16.1053059 35.3144932,16.1053059 C36.4140178,16.1053059 38.5518876,16.1085101 39.1006986,16.1053059 L39.1006986,9.05587273 L39.0974944,9.05587273 L39.0974944,9.05587273 Z"
+                        ></path>
+                      </g>
+                    </g>
+                  </svg>
+                  <span className="ml-2">Sign in with Facebook</span>
+                </button>
+              </div>
+            </div>
+            <div className="mt-5">
+              <button
+                className=" cursor-pointer py-2 px-4 bg-blue-600 hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-blue-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg"
+                type="submit"
+              >
+                Log In !
+              </button>
+            </div>
+            <div className="flex items-center justify-between mt-4">
+              <span className="w-1/5 border-b dark:border-gray-600 md:w-1/4"></span>
+              <a
+                className="text-xs text-gray-500 uppercase dark:text-gray-400 hover:underline cursor-pointer"
+                onClick={() => setModalType("register")}
+              >
+                Create an Account !
+              </a>
+              <span className="w-1/5 border-b dark:border-gray-400 md:w-1/4"></span>
+            </div>
+          </div>
+        </div>
+      </form>
+    </>
+  );
+};
+
+export default Login;
