8e613ff76df1419992fd848a92764f5d175edfb7
hayyantahirr <hayyantahir7@gmail.com>
2025-08-13 00:28:40 +0500

    Firebase set up

---
+++ src/config/Firebase/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+const firebaseConfig = {
+  apiKey: "AIzaSyCHXE96Te7gYyjoSAb94wXpYR2EjjiQsWY",
+  authDomain: "lumenverse-c8a01.firebaseapp.com",
+  projectId: "lumenverse-c8a01",
+  storageBucket: "lumenverse-c8a01.firebasestorage.app",
+  messagingSenderId: "917118818672",
+  appId: "1:917118818672:web:f6e4b9a5d7bdf7ec1997b5",
+  measurementId: "G-HW6959KCBH",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth };
