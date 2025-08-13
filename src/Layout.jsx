f4ceca756016c60cf1d33522e736a4f4e6a028c2
hayyantahirr <hayyantahir7@gmail.com>
2025-08-14 02:42:06 +0500

    Navbar added to the app

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { Outlet } from "react-router";
 
+import ResponsiveAppBar from "./components/Navbar";
+
 const Layout = () => {
   return (
     <>
+      <ResponsiveAppBar />
       <Outlet />
     </>
   );
