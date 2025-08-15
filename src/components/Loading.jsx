61610bd1a38997904d1a29f13805f257c66ccc6e
hayyantahirr <hayyantahir7@gmail.com>
2025-08-15 21:20:05 +0500

    Blogs Printed

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,12 +1,18 @@
 const Loading = () => {
   return (
     <>
-      <div className="text-center">
-        <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin border-yellow-500 mx-auto"></div>
-        <h2 className="text-zinc-900 dark:text-white mt-4">Loading...</h2>
-        <p className="text-zinc-600 dark:text-zinc-400">
-          Universe of light about to Unviel ....
-        </p>
+      <div className="flex flex-col min-h-screen">
+        {/* Navbar here */}
+
+        <div className="flex-grow flex flex-col justify-center items-center">
+          <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin border-yellow-500"></div>
+          <h2 className="text-zinc-900 dark:text-white mt-4">Loading...</h2>
+          <p className="text-zinc-600 dark:text-zinc-400">
+            Universe of light about to Unveil ....
+          </p>
+        </div>
+
+        {/* Footer here */}
       </div>
     </>
   );
--- src/screens/Blogs.jsx
+++ src/screens/Blogs.jsx
@@ -6,7 +6,7 @@ import Loading from "../components/Loading";
 
 const Blogs = () => {
   const [blog, setBlog] = useState([]);
- 
+
   async function getDatafromdb(params) {
     const q = query(collection(db, "Blogs"));
 
@@ -24,22 +24,22 @@ const Blogs = () => {
   }, []);
   return (
     <>
-      {blog ? (
-        blog.map((item) => {
-          return (
+      <div className="w-[80%] mx-auto gap-5 flex flex-wrap justify-center mt-15">
+        {blog?.length > 0 ? (
+          blog.map((item) => (
             <Card
               key={item.id}
               title={item.title}
               subText={item.subText}
-              article={item.Article}
+              article={item.Article.slice(0,100)}
               tags={item.tags}
               id={item.id}
             />
-          );
-        })
-      ) : (
-        <Loading />
-      )}
+          ))
+        ) : (
+          <Loading />
+        )}
+      </div>
     </>
   );
 };
