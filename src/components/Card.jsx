6764074a9fe55c94e9fc1b5409191c7af5059b9f
hayyantahirr <hayyantahir7@gmail.com>
2025-08-16 14:49:32 +0500

    Detail page setup !

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,10 @@
+import { useNavigate } from "react-router";
+
 const Card = ({ title, subText, article, tags, id }) => {
+const navigate = useNavigate()
+function handleClick() {
+  navigate(`/blogs/${id}`)
+}
   return (
     <>
       <div className="bg-white w-[30%] rounded-[30px] flex flex-col justify-center hover:shadow-lg min-h-[280px] dark:bg-gray-800 dark:text-white items-start relative group">
@@ -6,7 +12,7 @@ const Card = ({ title, subText, article, tags, id }) => {
           <div className="  text-2xl ">{title}</div>
 
           <div className=" text-left w-full mb-15">
-            <h2 className=" roboto-mono-500 text-gray-800 dark:text-white opacity-70 text-xl">
+            <h2 className=" roboto-mono-500 text-gray-800 dark:text-white opacity-20 text-l">
               {subText}
             </h2>
             <p className="mt-5 text-sm text-gray-500 dark:text-gray-300 mb-5">
@@ -16,11 +22,10 @@ const Card = ({ title, subText, article, tags, id }) => {
               🏷️ Tags : {tags}
             </p>
           </div>
-          <a href="#">
-            <div className="bg-gray-300 dark:bg-gray-700 w-[25%]  rounded-lg absolute bottom-0 left-0 m-4 flex justify-center items-center hover:ring-4 ring-gray-200 dark:ring-gray-400 hover:transition duration-700 ease-in-out">
-              <h1 className="text-center py-3">read more</h1>
-            </div>
-          </a>
+
+          <div onClick={handleClick} className="bg-gray-300 dark:bg-gray-700 w-[25%]  rounded-lg absolute bottom-0 left-0 m-4 flex justify-center items-center hover:ring-4 ring-gray-200 dark:ring-gray-400 hover:transition duration-700 ease-in-out cursor-pointer">
+            <h1 className="text-center py-3">Read More</h1>
+          </div>
         </div>
       </div>
     </>
--- src/screens/BlogDetail.jsx
+++ src/screens/BlogDetail.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { db } from "../config/Firebase/firebase";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import Loading from "../components/Loading";
 
 const BlogDetail = () => {
-  return (
-    <div>BlogDetail</div>
-  )
-}
+  const params = useParams();
+  const [blog, setBlog] = useState();
+  async function getDatafromdb() {
+    const q = query(collection(db, "Blogs"), where("id", "==", params.id));
 
-export default BlogDetail
+    const querySnapshot = await getDocs(q);
+    const alldocs = querySnapshot.docs.map((doc) => {
+      const data = doc.data();
+      return data;
+    });
+    setBlog(alldocs[0]);
+    console.log(alldocs);
+  }
+
+  useEffect(() => {
+    getDatafromdb();
+  }, []);
+  return (
+    <>
+      <div className="w-[80%] text-white mx-auto flex flex-col justify-center ">
+        <div className="">
+          <h1 className="text-3xl font-bold mt-10 mb-3 border-b-2 pb-5 border-gray-700">
+            <span className="opacity-40 italic text-yellow-400">Title : </span>
+            {blog?.title}
+          </h1>
+          <h2 className="text-lg opacity-20">
+            <span className="opacity-80 italic text-yellow-400">Subtext : </span>
+            {blog?.subText}
+          </h2>
+        </div>
+        <div className="border-b-2 pb-5 border-gray-700">
+          <h3 className="mt-10 opacity-40 italic  text-yellow-400">Article : </h3>
+          <h3 className=" mb-3 text-xl w-[90%] mx-auto opacity-70 text-slate-50">
+            {blog?.Article}
+          </h3>
+        </div>
+        <div className="flex  mt-5 opacity-70">
+          <span className=" italic text-yellow-400"> 🏷️ Tags :</span>
+          <h4 className="w-[90%] mx-auto">{blog?.tags}</h4>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default BlogDetail;
