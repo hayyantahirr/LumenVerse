1c0b1af950af4df8895a09f8f25dc5bef8fac4c2
hayyantahirr <hayyantahir7@gmail.com>
2025-08-17 22:56:44 +0500

    Icons added and app improved

--- src/components/BookMark.jsx
+++ src/components/BookMark.jsx
@@ -17,22 +17,18 @@ const BookMark = ({}) => {
       )}
       {/* The floating bubble modal */}
       {isOpen && (
-        <div className="fixed bottom-28 right-10 z-50 w-80 p-4 bg-white dark:bg-gray-800 shadow-xl rounded-2xl animate-scaleIn">
+        <div className=" fixed bottom-28 right-10 z-50 w-80 p-4 bg-[#083D77] shadow-xl rounded-2xl animate-scaleIn">
           <div className="text-sm text-gray-700 dark:text-gray-200">
-            <Bookmarks />
+            
+            <Bookmarks isOpen ={setIsOpen}/>
           </div>
-          <button
-            className="mt-3 px-4 py-2 bg-red-500 hover:bg-red-600 text-white text-sm rounded-lg"
-            onClick={() => setIsOpen(false)}
-          >
-            Close
-          </button>
         </div>
       )}
       {/* Floating button */}
       <button
+        title="Bookmarks"
         onClick={() => setIsOpen(true)}
-        className=" fixed bottom-10 right-10 cursor-pointer bg-gray-700 px-5 py-5 rounded-full text-white tracking-wider shadow-xl animate-bounce hover:animate-none z-10"
+        className=" fixed bottom-10 right-10 cursor-pointer bg-[#083D77] px-5 py-5 rounded-full text-white tracking-wider shadow-xl animate-bounce hover:animate-none active:animate-none z-10"
       >
         <svg
           viewBox="0 -0.5 25 25"
@@ -47,12 +43,12 @@ const BookMark = ({}) => {
             d="M18.507 19.853V6.034C18.5116 5.49905 18.3034 4.98422 17.9283 4.60277C17.5532 4.22131 17.042 4.00449 16.507 4H8.50705C7.9721 4.00449 7.46085 4.22131 7.08577 4.60277C6.7107 4.98422 6.50252 5.49905 6.50705 6.034V19.853C6.45951 20.252 6.65541 20.6407 7.00441 20.8399C7.35342 21.039 7.78773 21.0099 8.10705 20.766L11.907 17.485C12.2496 17.1758 12.7705 17.1758 13.113 17.485L16.9071 20.767C17.2265 21.0111 17.6611 21.0402 18.0102 20.8407C18.3593 20.6413 18.5551 20.2522 18.507 19.853Z"
             clipRule="evenodd"
             fillRule="evenodd"
-            fill="#ffffff"
+            fill="#38bdf8"
           ></path>
         </svg>
         {/* Count Badge */}
         {bookmarks.length > 0 && (
-          <span className="absolute -top-1 -right-1 bg-red-500 text-white text-xs font-bold px-2 py-1 rounded-full">
+          <span className="absolute -top-1 -right-1 bg-[#231F20] text-white text-xs font-bold px-2 py-1 rounded-full">
             {bookmarks.length}
           </span>
         )}
--- src/components/Bookmarks.jsx
+++ src/components/Bookmarks.jsx
@@ -1,30 +1,55 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { removeFromBookMark } from "../config/Redux/bmSlice";
+import { DEFAULT_VERSION } from "redux-persist";
+import { useNavigate } from "react-router";
 
-const Bookmarks = () => {
+const Bookmarks = ({ isOpen }) => {
   const bookMark = useSelector((state) => state.bookMark);
   const dispatch = useDispatch();
-  useEffect(() => {
-    console.log(bookMark);
-  }, []);
+const navigate = useNavigate();
+function handleClick (id){
+navigate (`/blogs/${id}`)
+isOpen(false)
+}
+
   return (
     <>
-      {bookMark
-        ? bookMark.map((item) => (
-            <div>
-              <p>{item.title}</p>
+      <div className="flex justify-center items-center  border-b-2 pb-2 border-gray-500">
+        <button
+          title="Minimize"
+          className="w-6 h-6 mb-7 text-white text-sm rounded-full cursor-pointer mr-auto"
+          onClick={() => isOpen(false)}
+        >
+          <img src="/Images/close.svg" alt="" />
+        </button>
+        <h1 className=" text-center font-bold text-2xl mr-12">
+          Your BookMarks
+        </h1>
+      </div>
+
+      {/* Container with fixed height and scroll */}
+      <div className="mt-4 max-h-[220px] min-h-[200px] overflow-y-auto ">
+        {bookMark && bookMark.length > 0 ? (
+          bookMark.map((item) => (
+            <div
+              key={item.id}
+              className="flex justify-between items-center mb-5  pb-2"
+
+            >
+              <p className="w-[90%] text-l hover:underline cursor-pointer" onClick={()=>{handleClick(item.id)}}>{item.title}</p>
               <button
-                onClick={() => {
-                  dispatch(removeFromBookMark(item.id));
-                }}
-                className="bg-red-500 p-2"
+                onClick={() => dispatch(removeFromBookMark(item.id))}
+                className="rounded-full mr-2"
               >
-                Remove BookMark
+                <img src="/Images/delete.svg" alt="" className="w-[25px]" />
               </button>
             </div>
           ))
-        : "no bookmarks"}
+        ) : (
+          <p className="text-center mt-4 text-gray-500">No bookmarks yet</p>
+        )}
+      </div>
     </>
   );
 };
