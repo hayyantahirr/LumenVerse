de6f4e551c1dc3867b80928e52b82205822ac9d8
hayyantahirr <hayyantahir7@gmail.com>
2025-08-17 17:43:43 +0500

    Store persisting done

--- src/config/Redux/store.js
+++ src/config/Redux/store.js
@@ -1,10 +1,33 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import bookMarkReducer from "./bmSlice";
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+import persistReducer from "redux-persist/es/persistReducer";
+import persistStore from "redux-persist/es/persistStore";
 
-const store = configureStore({
-  reducer: {
-    bookMark: bookMarkReducer,
-  },
-});
 
-export default store;
+
+const rootReducer = combineReducers({
+  bookMark: bookMarkReducer,
+})
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  // You can whitelist or blacklist specific slices here if needed
+  // whitelist: ['cart'] // Only persist cart
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false, // Very important for redux-persist
+    }),
+})
+
+export const persistor = persistStore(store)
+
+
+
