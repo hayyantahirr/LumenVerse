2d170d77716b1f487efe1ce3c825801cdcc2898d
hayyantahirr <hayyantahir7@gmail.com>
2025-08-20 17:26:00 +0500

    Login Register fixed

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -182,14 +182,7 @@ const Register = ({ setModalType }) => {
                   </button>
                 </div>
               </div>
-              <div className="text-right mb-4">
-                <a
-                  className="text-xs font-display font-semibold text-gray-500 hover:text-gray-600 cursor-pointer"
-                  href="#"
-                >
-                  Forgot Password?
-                </a>
-              </div>
+
               <div className="mt-5">
                 <button
                   className=" cursor-pointer py-2 px-4 bg-blue-600 hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-blue-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg"
